2641d26661f80163ec69728366839de4d021ea52
Justin Wayne Wilson <wilsonjustinwayne@gmail.com>
2018-06-14 16:28:06 -0500

    finished with conditionals

---
+++ js/conditionals.js
@@ -0,0 +1,209 @@
+(function () {
+    "use strict";
+
+    /**
+     * TODO:
+     * Write some JavaScript that uses a `confirm` dialog to ask the user if they
+     * would like to enter a number. If they click 'Ok', prompt the user for a
+     * number, then use 3 separate alerts to tell the user:
+     *
+     * - whether the number is even or odd
+     * - what the number plus 100 is
+     * - if the number is negative or positive
+     *
+     * if what the user enters is not a number, use an alert to tell them that, and
+     * do *not* display any of the above information.
+     *
+     * Can you refactor your code to use functions?
+     */
+
+    var numberConfirm = confirm("Would you like to enter a number?");
+    if (numberConfirm) {
+        var numberEntered = prompt("What number would you like to use?");
+
+        if (isNaN(parseInt(numberEntered))) {
+            alert("You did not give me a number!");
+        } else {
+            numberEntered = parseInt(numberEntered);
+
+            if (numberEntered % 2 === 1) {
+                alert("Your number is an odd number.");
+            } else if (numberEntered % 2 === 0) {
+                alert("Your number is an even number.");
+            }
+
+            alert("Adding 100 to your number makes it " + (numberEntered + 100));
+
+            if (numberEntered > 0) {
+                alert("You gave me a positive number");
+            } else if (numberEntered < 0) {
+                alert("You gave me a negative number");
+            } else {
+                alert("You gave me 0, which is neither a positive nor a negative number");
+            }
+        }
+    }
+
+    /* ########################################################################## */
+
+    /**
+     * TODO:
+     * Create a function named `analyzeColor` that accepts a string that is a color
+     * name as input. This function should return a message that related to that
+     * color. Only worry about the colors defined above, if the color passed is not
+     * one of the ones defined above, return a message that says so
+     *
+     * Example:
+     *  > analyzeColor('blue') // returns "blue is the color of the sky"
+     *  > analyzeColor('red') // returns "Strawberries are red"
+     *  > analyzeColor('cyan') // returns "I don't know anything about cyan"
+     *
+     * You should use an if-else-if-else block to return different messages.
+     *
+     * Test your function by passing various string literals to it and
+     * console.logging the function's return value
+     */
+
+// Don't change the next two lines!
+// These lines create two variables for you:
+// - `colors`: a list of the colors of the rainbow
+// - `randomColor`: contains a single random color value from the list (this
+//                  will contain a different color everytime the page loads)
+    var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+    var randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+    function analyzeColor(color) {
+        if (color === "red") {
+            console.log("apples are red");
+        } else if (color === "orange") {
+            console.log("oranges are orange");
+        } else if (color === "yellow") {
+            console.log("bananas are yellow");
+        } else if (color === "green") {
+            console.log("limes are green");
+        } else if (color === "blue") {
+            console.log("blueberries are blue");
+        } else if (color === "indigo") {
+            console.log("indigo is a color");
+        } else if (color === "violet") {
+            console.log("violet is a color as well");
+        } else {
+            console.log("...ya there is no way that " + color + " is a color");
+        }
+    }
+
+
+    /**
+     * TODO:
+     * Pass the `randomColor` variable to your function and console.log the results.
+     * You should see a different message everytime you refresh the page
+     */
+
+    analyzeColor(randomColor);
+
+    /**
+     * TODO:
+     * Refactor your above function to use a switch-case statement
+     */
+
+    var color;
+    switch (randomColor) {
+        case "red":
+            console.log("apples are red");
+            break;
+        case "orange":
+            console.log("oranges are orange");
+            break;
+        case "yellow":
+            console.log("bananas are yellow");
+            break;
+        case "green":
+            console.log("limes are green");
+            break;
+        case "blue":
+            console.log("blueberries are blue");
+            break;
+        case "indigo":
+            console.log("indigo is a color");
+            break;
+        case "violet":
+            console.log("violet is a color as well");
+            break;
+        default:
+            console.log(color + " is not a color");
+            break;
+    }
+
+    /**
+     * TODO:
+     * Prompt the user for a color when the page loads, and pass the input from the
+     * user to your `analyzeColor` function. Alert the return value from your
+     * function to show it to the user.
+     */
+
+    analyzeColor(prompt("Name a color please. (all lowercase)"));
+
+
+    /* ########################################################################## */
+
+    /**
+     * TODO:
+     * Suppose there's a promotion in Walmart, each customer is given a randomly
+     * generated "lucky number" between 0 and 5. If your lucky number is 0 you have
+     * no discount, if your lucky number is 1 you'll get a 10% discount, if it's 2,
+     * the discount is 25%, if it's 3, 35%, if it's 4, 50%, and if it's 5 you'll get
+     * all for free!.
+     *
+     * Write a function named `calculateTotal` that accepts a lucky number and total
+     * amount, and returns the discounted price.
+     *
+     * Example:
+     * calculateTotal(0, 100) // returns 100
+     * calculateTotal(4, 100) // returns 50
+     * calculateTotal(5, 100) // returns 0
+     *
+     * Test your function by passing it various values and checking for the expected
+     * return value.
+     */
+    function calculateTotal(luckyNumber, subTotalAmount) {
+        var discountPercentage;
+        switch (luckyNumber) {
+            case 0:
+                discountPercentage = 0;
+                break;
+            case 1:
+                discountPercentage = .1;
+                break;
+            case 2:
+                discountPercentage = .25;
+                break;
+            case 3:
+                discountPercentage = .35;
+                break;
+            case 4:
+                discountPercentage = .50;
+                break;
+            case 5:
+                discountPercentage = 1;
+                break;
+            default:
+                break;
+        }
+        var totalAmount = subTotalAmount - (subTotalAmount * discountPercentage);
+        alert("Your Lucky Number: " + luckyNumber);
+        alert("Your subtotal: $" + subTotalAmount);
+        alert("Your total: $" + totalAmount);
+    }
+
+    /**
+     * TODO:
+     * Uncomment the line below to generate a random number between 0 and 6.
+     * Prompt the user for their total bill, then use your `calculateTotal` function
+     * and alerts to display to the user what their lucky number was, what their
+     * price before the discount was, and what their price after the discount is.
+     */
+// Generate a random number between 0 and 6
+var luckyNumber = Math.floor(Math.random() * 6);
+calculateTotal(luckyNumber, prompt("What is your bill total?"));
+
+})();
--- js/functions.js
+++ js/functions.js
@@ -1,220 +1,223 @@
-(function () {
+// (function () {
 
 
     "use strict";
 
-    /**
-     * TODO:
-     * Create a function called 'sayHello' that takes a parameter 'name'.
-     * When called, the function should return a message that says hello to the passed in name.
-     *
-     * Example
-     * > sayHello("codeup") // returns "Hello, codeup!"
-     */
-
-    function sayHello(name) {
-        return "Hello, " + name + "!";
-    }
-
-    /**
-     * TODO:
-     * Call the function 'sayHello' and pass your name as a string literal argument.
-     * Store the result of the function call in a variable named 'helloMessage'.
-     *
-     * console.log 'helloMessage' to check your work
-     */
-
-    var helloMessage = sayHello("Dewie");
-    console.log(helloMessage);
-
-    /**
-     * TODO:
-     * Store your name as a string in a variable named 'myName', and pass that
-     * variable to the 'sayHello' function. You should see the same output in the
-     * console.
-     */
-
-    var myName = "Dewie";
-    sayHello(myName);
-    console.log(helloMessage);
-    // Don't modify the following line, it generates a random number between 1 and 3
-    // and stores it in a variable named random
-    var random = Math.floor((Math.random() * 3) + 1);
-
-    /**
-     * TODO:
-     * Create a function called 'isTwo' that takes a number as a parameter.
-     * The function should return a boolean value based on whether or not the passed
-     * number is the number 2.
-     *
-     * Example
-     * > isTwo(1) // returns false
-     * > isTwo(2) // returns true
-     * > isTwo(3) // returns false
-     *
-     * Call the function 'isTwo' passing the variable 'random' as a argument.
-     *
-     * console.log *outside of the function* to check your work (you should see a
-     * different result everytime you refresh the page if you are using the random
-     * number)
-     */
-
-    function isTwo(number) {
-        return number == 2;
-    }
-
-    console.log(isTwo(random));
-
-    /**
-     * TODO:
-     * Create a function named 'calculateTip' to calculate a tip on a bill at a
-     * restaurant. The function should accept a tip percentage and the total of the
-     * bill, and return the amount to tip
-     *
-     * Examples:
-     * > calculateTip(0.20, 20) // returns 4
-     * > calculateTip(0.25, 25.50) // returns 6.37
-     * > calculateTip(0.15, 33.42) // returns 5.013
-     */
-
-    function calculateTip(tipPercentage, totalBillAmount) {
-        return totalBillAmount * (tipPercentage / 100);
-    }
-
-    /**
-     * TODO:
-     * Use prompt and alert in combination with your calculateTip function to
-     * prompt the user for the bill total and a percentage they would like to tip,
-     * then display the dollar amount they should tip
-     */
-
-    var tipAmount = calculateTip(prompt("What is the percentage of tip you would like to use?"), prompt("What is the total bill amount?"));
-    alert("The amount you should tip: $" + tipAmount.toFixed(2));
-
-    /**
-     * TODO:
-     * Create a function named `applyDiscount`. This function should accept a price
-     * (before a discount is applied), and a discount percentage (a number between 0
-     * and 1). It should return the result of applying the discount to the original
-     * price.
-     *
-     * Example:
-     * > var originalPrice = 100;
-     * > var dicountPercent = .2; // 20%
-     * > applyDiscount(originalPrice, dicountPercent) // 80
-     *
-     * > applyDiscount(45.99, 0.12) // 40.4712
-     */
-
-    function applyDiscount(originalPrice, discountPercentage) {
-        var discountAmount = originalPrice * (discountPercentage / 100);
-        return originalPrice - discountAmount;
-    }
-
-    var discountedPrice = applyDiscount(prompt("What is the original price you would like to use?"), prompt("What is the discount percentage?"));
-    alert("The discounted price: $" + discountedPrice.toFixed(2));
-
-
-
-
-    // ================ SIMPLE FUNCTION DRILLS
-
-
-    // 1) Make a function called returnTwo() that returns the number 2 when called
-    // ---Test this function with console.log(returnTwo())
-
-    function returnTwo() {
-        return 2;
-    }
-    console.log(returnTwo());
-
-    // 2) Make a function called returnName() that returns the string of your name
-    // ---Test this function with console.log(returnName())
-
-    function returnName() {
-        return "Dewie";
-    }
-    console.log(returnName());
-
-    // 3) Make a function called addThree() which takes in a number input and returns the number plus 3.
-    // ---Test this function with console.log(addThree(5))
-
-    function addThree(number) {
-        return number += 3;
-    }
-    console.log(addThree(5));
-
-    // 4) Make a function called sayString() which returns the string input passed in.
-    // ---Test this function with console.log(sayString())
-
-    function sayString(string) {
-        return string;
-    }
-    console.log(sayString("Poopadoop"));
-
-    // 5) Make a function called sayHowdy() which console.logs the string “Howdy!”
-    // ---Test this function by directly calling sayHowdy()
-    // ---Remember this function does not need a defined return value
-
-    function sayHowdy() {
-        console.log("Howdy!");
-    }
-    sayHowdy();
-
-
-    // ================ CHALLENGE FUNCTION DRILLS
-
-    // Write a function called `identity(input)` that takes in an argument called input and returns that input.
-
-    function identity(input) {
-        return input;
-    }
-
-    // Write a function called `getRandomNumber(min, max)` that returns a random number between min and max values sent to that function call.
-
-    function getRandomNumber(min,max) {
-        return Math.floor(Math.random() * (max - min)) + min;
-    }
-
-    // Write a function called `first(input)` that returns the first character in the provided string.
-
-    function first(input) {
-        return input.charAt(0);
-    }
-
-    // Write a fuction called `last(input)` that returns the last character of a string
-
-    function last(input) {
-        return input.charAt(input.length - 1);
-    }
-
-    // Write a function called `rest(input)` that returns everything but the first character of a string.
-
-    function rest(input) {
-        return input.substring(1);
-    }
-
-    // Write a function called `reverse(input)` that takes a string and returns it reversed.
-
-    function reverse(input) {
-        var stringToArray = input.split("");
-        var reversedArray = stringToArray.reverse();
-        var reversedInput = reversedArray.join("");
-        return reversedInput;
-    }
-
-    // Write a function called `isNumeric(input)` that takes an input and returns a boolean if the input is numeric.
-
-    function isNumeric(input) {
-        var isInputNumeric = isNaN(input);
-        return isInputNumeric;
-    }
-
-    // Write a function called `count(input)` that takes in a string and returns the number of characters.
-
-    function count(input) {
-        return input.length;
-    }
+    // /**
+    //  * TODO:
+    //  * Create a function called 'sayHello' that takes a parameter 'name'.
+    //  * When called, the function should return a message that says hello to the passed in name.
+    //  *
+    //  * Example
+    //  * > sayHello("codeup") // returns "Hello, codeup!"
+    //  */
+    //
+    // function sayHello(name) {
+    //     return "Hello, " + name + "!";
+    // }
+    //
+    // /**
+    //  * TODO:
+    //  * Call the function 'sayHello' and pass your name as a string literal argument.
+    //  * Store the result of the function call in a variable named 'helloMessage'.
+    //  *
+    //  * console.log 'helloMessage' to check your work
+    //  */
+    //
+    // var helloMessage = sayHello("Dewie");
+    // console.log(helloMessage);
+    //
+    // /**
+    //  * TODO:
+    //  * Store your name as a string in a variable named 'myName', and pass that
+    //  * variable to the 'sayHello' function. You should see the same output in the
+    //  * console.
+    //  */
+    //
+    // var myName = "Dewie";
+    // sayHello(myName);
+    // console.log(helloMessage);
+    // // Don't modify the following line, it generates a random number between 1 and 3
+    // // and stores it in a variable named random
+    // var random = Math.floor((Math.random() * 3) + 1);
+    //
+    // /**
+    //  * TODO:
+    //  * Create a function called 'isTwo' that takes a number as a parameter.
+    //  * The function should return a boolean value based on whether or not the passed
+    //  * number is the number 2.
+    //  *
+    //  * Example
+    //  * > isTwo(1) // returns false
+    //  * > isTwo(2) // returns true
+    //  * > isTwo(3) // returns false
+    //  *
+    //  * Call the function 'isTwo' passing the variable 'random' as a argument.
+    //  *
+    //  * console.log *outside of the function* to check your work (you should see a
+    //  * different result everytime you refresh the page if you are using the random
+    //  * number)
+    //  */
+    //
+    // function isTwo(number) {
+    //     return number == 2;
+    // }
+    //
+    // console.log(isTwo(random));
+    //
+    // /**
+    //  * TODO:
+    //  * Create a function named 'calculateTip' to calculate a tip on a bill at a
+    //  * restaurant. The function should accept a tip percentage and the total of the
+    //  * bill, and return the amount to tip
+    //  *
+    //  * Examples:
+    //  * > calculateTip(0.20, 20) // returns 4
+    //  * > calculateTip(0.25, 25.50) // returns 6.37
+    //  * > calculateTip(0.15, 33.42) // returns 5.013
+    //  */
+    //
+    // function calculateTip(tipPercentage, totalBillAmount) {
+    //     return totalBillAmount * (tipPercentage / 100);
+    // }
+    //
+    // /**
+    //  * TODO:
+    //  * Use prompt and alert in combination with your calculateTip function to
+    //  * prompt the user for the bill total and a percentage they would like to tip,
+    //  * then display the dollar amount they should tip
+    //  */
+    //
+    // var tipAmount = calculateTip(prompt("What is the percentage of tip you would like to use?"), prompt("What is the total bill amount?"));
+    // alert("The amount you should tip: $" + tipAmount.toFixed(2));
+    //
+    // /**
+    //  * TODO:
+    //  * Create a function named `applyDiscount`. This function should accept a price
+    //  * (before a discount is applied), and a discount percentage (a number between 0
+    //  * and 1). It should return the result of applying the discount to the original
+    //  * price.
+    //  *
+    //  * Example:
+    //  * > var originalPrice = 100;
+    //  * > var dicountPercent = .2; // 20%
+    //  * > applyDiscount(originalPrice, dicountPercent) // 80
+    //  *
+    //  * > applyDiscount(45.99, 0.12) // 40.4712
+    //  */
+    //
+    // function applyDiscount(originalPrice, discountPercentage) {
+    //     var discountAmount = originalPrice * (discountPercentage / 100);
+    //     return originalPrice - discountAmount;
+    // }
+    //
+    // var discountedPrice = applyDiscount(prompt("What is the original price you would like to use?"), prompt("What is the discount percentage?"));
+    // alert("The discounted price: $" + discountedPrice.toFixed(2));
+    //
+    //
+    // // ================ SIMPLE FUNCTION DRILLS
+    //
+    //
+    // // 1) Make a function called returnTwo() that returns the number 2 when called
+    // // ---Test this function with console.log(returnTwo())
+    //
+    // function returnTwo() {
+    //     return 2;
+    // }
+    //
+    // console.log(returnTwo());
+    //
+    // // 2) Make a function called returnName() that returns the string of your name
+    // // ---Test this function with console.log(returnName())
+    //
+    // function returnName() {
+    //     return "Dewie";
+    // }
+    //
+    // console.log(returnName());
+    //
+    // // 3) Make a function called addThree() which takes in a number input and returns the number plus 3.
+    // // ---Test this function with console.log(addThree(5))
+    //
+    // function addThree(number) {
+    //     return number += 3;
+    // }
+    //
+    // console.log(addThree(5));
+    //
+    // // 4) Make a function called sayString() which returns the string input passed in.
+    // // ---Test this function with console.log(sayString())
+    //
+    // function sayString(string) {
+    //     return string;
+    // }
+    //
+    // console.log(sayString("Poopadoop"));
+    //
+    // // 5) Make a function called sayHowdy() which console.logs the string “Howdy!”
+    // // ---Test this function by directly calling sayHowdy()
+    // // ---Remember this function does not need a defined return value
+    //
+    // function sayHowdy() {
+    //     console.log("Howdy!");
+    // }
+    //
+    // sayHowdy();
+    //
+    //
+    // // ================ CHALLENGE FUNCTION DRILLS
+    //
+    // // Write a function called `identity(input)` that takes in an argument called input and returns that input.
+    //
+    // function identity(input) {
+    //     return input;
+    // }
+    //
+    // // Write a function called `getRandomNumber(min, max)` that returns a random number between min and max values sent to that function call.
+    //
+    // function getRandomNumber(min, max) {
+    //     return Math.floor(Math.random() * (max - min)) + min;
+    // }
+    //
+    // // Write a function called `first(input)` that returns the first character in the provided string.
+    //
+    // function first(input) {
+    //     return input.charAt(0);
+    // }
+    //
+    // // Write a fuction called `last(input)` that returns the last character of a string
+    //
+    // function last(input) {
+    //     return input.charAt(input.length - 1);
+    // }
+    //
+    // // Write a function called `rest(input)` that returns everything but the first character of a string.
+    //
+    // function rest(input) {
+    //     return input.substring(1);
+    // }
+    //
+    // // Write a function called `reverse(input)` that takes a string and returns it reversed.
+    //
+    // function reverse(input) {
+    //     var stringToArray = input.split("");
+    //     var reversedArray = stringToArray.reverse();
+    //     var reversedInput = reversedArray.join("");
+    //     return reversedInput;
+    // }
+    //
+    // // Write a function called `isNumeric(input)` that takes an input and returns a boolean if the input is numeric.
+    //
+    // function isNumeric(input) {
+    //     var isInputNumeric = isNaN(input);
+    //     return isInputNumeric;
+    // }
+    //
+    // // Write a function called `count(input)` that takes in a string and returns the number of characters.
+    //
+    // function count(input) {
+    //     return input.length;
+    // }
 
     // Write a function called `add(a, b)` that returns the sum of a and b
 
@@ -262,7 +265,19 @@
     // Write a function called doMath(operator, a, b) that takes 3 parameters. The first parameter is the name of the math function you want to apply. a and b are the two numbers to run that function on.
 
     function doMath(operator, a, b) {
-        return (a operator b);
+        // switch (operator) {
+            // case "add"/*, "subtract", "multiply", "divide", "remainder" */:
+                console.log(typeof operator + "," + operator.valueOf());
+                var functionName = window[operator];
+                console.log(typeof functionName + "," + functionName.valueOf());
+                return functionName(parseInt(a), parseInt(b));
+                // break;
+            // default :
+            //     return alert("You did not spell the mathematic function correctly, or you put in an option not available.")
+        // }
     }
+console.log(doMath(prompt("Please type the operator you would like to use in this equation. (ie. add, subtract, multiply, divide, remainder)"),
+    prompt("Please type the first number in your equation."),
+    prompt("Please type the second number in your equation.")));
 
-})();
+// })();
